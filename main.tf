ca002619bd167bb306cbd91b21aad825cf907060
Michael Oketch <48472370+Gumbeoketch@users.noreply.github.com>
2024-10-10 21:23:18 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,3 +12,44 @@ data "aws_availability_zones" "available" {}
 locals {
   cluster_name = var.clusterName
 }
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  value = module.eks.cluster_certificate_authority_data
+}
+
+resource "aws_lb" "app_lb" {
+  name               = "app-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = module.vpc.public_subnets
+
+  enable_deletion_protection = false
+}
+
+resource "aws_security_group" "lb_sg" {
+  vpc_id = module.vpc.vpc_id  # Ensure this references the correct VPC
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP access
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow all outbound traffic
+  }
+
+  tags = {
+    Name = "lb-sg"
+  }
+}
+
