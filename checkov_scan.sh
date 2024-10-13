5c8299e89ea3f0d3464b996c1cd53cf46661509c
Michael Oketch <48472370+Gumbeoketch@users.noreply.github.com>
2024-10-13 11:58:48 +0300

    Update checkov_scan.sh

--- checkov_scan.sh
+++ checkov_scan.sh
@@ -4,16 +4,16 @@
 if ! command -v checkov &> /dev/null
 then
     echo "Checkov is not installed. Installing..."
-    pip install checkov
+    pip3 install checkov
 fi
 
 # Run Checkov scan on the current directory
 echo "Running Checkov security scan on Terraform files..."
-checkov --directory .
+checkov --directory . --outouy-file tf_scan.json
 
 # Check if Checkov exited with non-zero (indicating failures)
 if [[ $? -ne 0 ]]; then
-    echo "Security vulnerabilities found. Fix them before proceeding."
+    echo "misconfiguration or vulnerabilities found. Fix them before proceeding."
     exit 1
 else
     echo "No security vulnerabilities found."
